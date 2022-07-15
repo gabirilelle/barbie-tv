286f3f3d041d09229d1ecdf58109b27273f68e39
gabirilelle <gaf@academy.cin.ufpe.br>
2022-07-15 10:14:21 -0300

    adiciona key e função de chamar a key

---
+++ Barbie tv/Model/Movie+API.swift
@@ -0,0 +1,26 @@
+//
+//  Movie+API.swift
+//  FilmADA
+//
+//  Created by Francisco Soares Neto on 13/07/22.
+//
+
+import Foundation
+
+extension Movie {
+
+    // MARK: - Recuperando a chave da API de um arquivo
+    static var apiKey: String {
+        guard let url = Bundle.main.url(forResource: "TMDB-Info", withExtension: "plist") else {
+            fatalError("Couldn't find api key configuration file.")
+        }
+        guard let plist = try? NSDictionary(contentsOf: url, error: ()) else {
+            fatalError("Couldn't interpret api key configuration file as plist.")
+        }
+        guard let key = plist["API_KEY"] as? String else {
+            fatalError("Couldn't find an api key in its configuration file.")
+        }
+        return key
+    }
+}
+
